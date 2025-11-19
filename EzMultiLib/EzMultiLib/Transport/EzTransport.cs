160df0b3d978d0bff1742498e4943284f973e6c1
Conner Sheehan <91071002+csheehan18@users.noreply.github.com>
2025-11-19 12:45:08 -0500

    Initial Commit of Structure
    - Did include first iteration of Transport as I wanted to have a foundation laid out

---
+++ EzMultiLib/EzMultiLib/Transport/EzTransport.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace EzMultiLib.Transport
+{
+    public class EzTransport
+    {
+        private UdpClient udp;
+		public event Action<IPEndPoint, byte[]>? OnData;
+
+		public EzTransport(int port)
+        {
+            udp = new UdpClient(port);
+            AcceptPackets();
+        }
+
+        private async void AcceptPackets()
+        {
+            while (true)
+            {
+                UdpReceiveResult result = await udp.ReceiveAsync();
+                OnData?.Invoke(result.RemoteEndPoint, result.Buffer);
+            }
+        }
+
+        public void Send(IPEndPoint endpoint, byte[] data)
+        {
+            udp.Send(data, data.Length, endpoint);
+        }
+
+        public void Dispose()
+        {
+            udp.Dispose();
+        }
+    }
+}
